34c5cdcd21f32f015fb8c94e61856a30f2f96a84
Srinjoyee_Dey <deysrinjoyee28@gmail.com>
2025-04-30 19:58:16 +0530

    main.py

--- main.py
+++ main.py
@@ -43,22 +43,3 @@ sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
 
 print(playlist["external_urls"]["spotify"])
 
-# sp = spotipy.Spotify(
-#     auth_manager=SpotifyOAuth(
-#         scope="playlist-modify-private",
-#         redirect_uri="https://example.com",
-#         client_id=CLIENT_ID,
-#         client_secret= CLIENT_SECRET,
-#         show_dialog=True,
-#         cache_path="token.txt"
-#     )
-# )
-# user_id = sp.current_user()["id"]
-
-# song_uris = {"access_token": "BQDyGwp3A6yFIBXFzGE2DZDZVkvW840p-6cgQ36_KejXz_uTBH8Tr-L2NXp_H-eLgBDukByiNp5PBfNPyQ1xZTpvVjK8iNeN63w3mURGE8uQVU5GYhALbGC55W15T4BSCrTg1xKpGDvT_iIkLE6l_oH17ypowRYBmKUGPiZpDQbhDQwJElV3kyDaR_q3AUuMH5eRYfKkf9QBeyNHe1oG1z1fUMS4zKkf4ixYCeihln5HZO3bsEfO_kzUvkm9WwrtfUcJlMll7A", "token_type": "Bearer", "expires_in": 3600, "refresh_token": "AQAARXxPbs8MYMZduPG-qdILsXxdAdXpbXYkAbXAsxDZuMVWb-FCmTCnJfYAiAsnk0HgOf4YmRSgKCYUHYlCJhaQ042t7rpO_Jq7RzbS3px4ewxjj7Ev_4OEoVflfBZmLGE", "scope": "playlist-modify-private", "expires_at": 1746024074}
-# playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-# # print(playlist)
-
-# sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
-
-
