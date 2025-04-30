647e3a24d7d08400090f29e5650047dd0e72227e
Srinjoyee_Dey <deysrinjoyee28@gmail.com>
2025-04-30 19:51:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID="CLIENT_ID"
+CLIENT_SECRET="CLIENT_SECRET"
+
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+header={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+url=("https://www.billboard.com/charts/hot-100/"+date)
+
+response=requests.get(url= url, headers=header)
+soup=BeautifulSoup(response.text,"html.parser")
+
+song_names_spans=soup.select("li ul li h3")
+song_names=[song.getText().strip() for song in song_names_spans]
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-private",redirect_uri="https://example.com",
+                    client_id=CLIENT_ID,client_secret=CLIENT_SECRET,show_dialog=True,cache_path="token.txt",username="Tinna Dey"))
+
+user_id=sp.current_user()["id"]
+
+# song_names = ["Stay","Blinding","Lights","Bad Habits"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+print(playlist["external_urls"]["spotify"])
+
+# sp = spotipy.Spotify(
+#     auth_manager=SpotifyOAuth(
+#         scope="playlist-modify-private",
+#         redirect_uri="https://example.com",
+#         client_id=CLIENT_ID,
+#         client_secret= CLIENT_SECRET,
+#         show_dialog=True,
+#         cache_path="token.txt"
+#     )
+# )
+# user_id = sp.current_user()["id"]
+
+# song_uris = {"access_token": "BQDyGwp3A6yFIBXFzGE2DZDZVkvW840p-6cgQ36_KejXz_uTBH8Tr-L2NXp_H-eLgBDukByiNp5PBfNPyQ1xZTpvVjK8iNeN63w3mURGE8uQVU5GYhALbGC55W15T4BSCrTg1xKpGDvT_iIkLE6l_oH17ypowRYBmKUGPiZpDQbhDQwJElV3kyDaR_q3AUuMH5eRYfKkf9QBeyNHe1oG1z1fUMS4zKkf4ixYCeihln5HZO3bsEfO_kzUvkm9WwrtfUcJlMll7A", "token_type": "Bearer", "expires_in": 3600, "refresh_token": "AQAARXxPbs8MYMZduPG-qdILsXxdAdXpbXYkAbXAsxDZuMVWb-FCmTCnJfYAiAsnk0HgOf4YmRSgKCYUHYlCJhaQ042t7rpO_Jq7RzbS3px4ewxjj7Ev_4OEoVflfBZmLGE", "scope": "playlist-modify-private", "expires_at": 1746024074}
+# playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# # print(playlist)
+
+# sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+
